097911b9b04c138de4cc347e12f8186111bbcb1a
JupiterSmokes <daniel-albizurezalpirez@cunoc.edu.gt>
2019-10-12 01:56:20 -0600

    Analizador funcionando correctamente

--- analisis.js
+++ analisis.js
@@ -1,13 +1,14 @@
 //Objeto de forma matricial que contiene las transicines del lenguaje
 var transitions = {
-    0:{'l':1,'n':2, "op":4, "op2":5, '=':5, 'a':6, 's':7 ,'e':8},
-    1:{'l':1, 'n':1, 'e':8, "tipo":"Identificador"},
-    2:{'n':2, '.':3,'e':8, "tipo":"Numero"},
-    3:{'n':2, 'e':8},
-    4:{'e':8, "tipo":"Operador"},
-    5:{'=':4, 'e':8,"tipo":"Operador"},
-    6:{'e':8, "tipo":"Agrupacion"},
-    7:{'e':8, "tipo":"Signo"},
+    0:{'l':1,'n':2, "op":4, "op2":5, '=':5, 'a':6, 's':7 , '.':8,'e':8},
+    //'l':1,'n':2, "op":4, "op2":5, '=':5, 'a':6, 's':7 ,'e':8
+    1:{'l':1, 'n':1, "op":8, "op2":8, '=':8, 'a':8, 's':8, '.':8,'e':8, "tipo":"Identificador"},
+    2:{'l':8, 'n':2, "op":8, "op2":8, '=':8, 'a':8, 's':8, '.':3,'e':8, "tipo":"Numero"},
+    3:{'l':8, 'n':2, "op":8, "op2":8, '=':8, 'a':8, 's':8, '.':8,'e':8},
+    4:{'l':8, 'n':8, "op":8, "op2":8, '=':8, 'a':8, 's':8, '.':8, 'e':8, "tipo":"Operador"},
+    5:{'l':8, 'n':2, "op":8, "op2":8, '=':4, 'a':8, 's':8, '.':8, 'e':8,"tipo":"Operador"},
+    6:{'l':8, 'n':2, "op":8, "op2":8, '=':8, 'a':8, 's':8, '.':8,'e':8, "tipo":"Agrupacion"},
+    7:{'l':8, 'n':2, "op":8, "op2":8, '=':8, 'a':8, 's':8, '.':8,'e':8, "tipo":"Signo"},
     8:{"tipo":"Error"}
 };
 var flotante = false;
@@ -30,11 +31,13 @@ var currentState;
 var currentCharGroup;
 var token;
 analize = (word) => {
+    var message;
     var c;
     length = word.length;
     currentState = 0;
     for (var i=0; i<length;i++ ) {
         c = word.charCodeAt(i);
+        currentCol++;
         currentCharGroup = charGroup(c);
 /*        console.log(c);
         console.log(currentCharGroup);
@@ -47,16 +50,18 @@ analize = (word) => {
     }
     if (!acceptanceStates.includes(currentState)) {
         console.log(currentCharGroup + currentState)
-        console.log("Error")
+        message = "Error";
+        fillTable("tablaErrores",2,[word, currentRow +":"+currentCol])
     } 
     else if(acceptanceStates.includes(currentState)) {
-        var message = "Se ha hallado un: " + transitions[currentState]["tipo"];
+        message = "Se ha hallado un: " + transitions[currentState]["tipo"];
         if (flotante) {
             message += " flotante";
             flotante = false;
         }
-        console.log(message);
-    }  
+        fillTable("tablaPatrones",3,[word, transitions[currentState]["tipo"], currentRow +":"+currentCol])
+    }
+    console.log(message + " " + word);  
 };
 evaluate = (state,char) => {return transitions[state][char]};
 
@@ -65,7 +70,7 @@ charGroup = (char) =>  {
         return 'l';
     } else if (char>=48 && char<=57) {
         return 'n';
-    } else if (char==46) {
+    } else if (char==46 && currentState == 2) {
         flotante = true;
         return '.';
     } else if ((char>=42 && char<=43) || (char==45) || (char==47) || (char==37)) {
@@ -74,7 +79,7 @@ charGroup = (char) =>  {
         return "op2";
     } else if (char==61) {
         return '=';
-    } else if ((char>=40 && char <=40) || (char==123 || char == 125)) {
+    } else if ((char>=40 && char <=41) || (char==123 || char == 125)) {
         return 'a'
     } else if (char == 34 || char == 59) {
         return 's';
@@ -83,3 +88,13 @@ charGroup = (char) =>  {
         return 'e';
     }
 }; 
+
+fillTable = (tableName, cells, data) =>{
+    var table = document.getElementById(tableName);
+    var new_Row = table.insertRow(-1);
+    for (let i = 0; i < cells; i++) {
+        var cell = new_Row.insertCell(i)
+        cell.innerHTML = data[i];
+        
+    }
+}
---
+++ functions.js
@@ -0,0 +1,37 @@
+var text;
+var currentWord;
+var currentRow;
+var currentCol;
+var rows;
+
+getText = () => {
+    text = document.getElementById("textA").value;
+    rows = text.split('\n');
+    for (let i = 0; i < rows.length; i++) {
+        words = rows[i].trim().split(' ');
+        currentRow = i+1;
+        currentCol = 1;
+        for (let j = 0; j < words.length; j++) {
+            analize(words[j]);
+            console.log(currentRow,currentCol-1);
+            currentCol++;
+        }        
+    }
+}
+
+file =() => {
+    document.getElementById("inputFile").
+    addEventListener("change", readFile, false);
+}
+readFile = (e)=> {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        text = e.target.result;
+        document.getElementById("textA").innerHTML = text;
+    };
+    reader.readAsText(file);
+}
